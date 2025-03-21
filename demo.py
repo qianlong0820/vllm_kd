594b40c6c8e90dcfd13d3437d7540db1b513e620
Kevin du <kevinonedu@gmail.com>
2025-03-21 16:51:38 +0000

    gm

---
+++ demo.py
@@ -0,0 +1,41 @@
+from transformers import AutoTokenizer, BitsAndBytesConfig, Gemma3ForCausalLM
+
+import torch
+
+model_id = "google/gemma-3-1b-it"
+
+quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+
+model = Gemma3ForCausalLM.from_pretrained(
+    model_id, quantization_config=quantization_config
+).eval()
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+messages = [
+    [
+        {
+            "role": "system",
+            "content": [{"type": "text", "text": "You are a helpful assistant."},]
+        },
+        {
+            "role": "user",
+            "content": [{"type": "text", "text": "Write a poem on Hugging Face, the company"},]
+        },
+    ],
+]
+inputs = tokenizer.apply_chat_template(
+    messages,
+    add_generation_prompt=True,
+    tokenize=True,
+    return_dict=True,
+    return_tensors="pt",
+).to(model.device).to(torch.bfloat16)
+
+
+with torch.inference_mode():
+    outputs = model.generate(**inputs, max_new_tokens=64)
+
+outputs = tokenizer.batch_decode(outputs)
+
+print(outputs)
